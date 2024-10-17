8b6690c5090d98b70c5f534860f3daeb3f98e32b
Joeee167 <youssefgamer037@gmail.com>
2024-10-16 23:03:59 -0400

    Part2

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,14 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import RegisterPanel from "./components/Register/Register"; // Import the correct component for registration
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      <Route path="/register" element={<RegisterPanel />} /> {/* Correct component for registration */}
     </Routes>
   );
 }
+
 export default App;
